ad31c9f823eade00a37d045f9fdc1c6656fd1e7d
Salma Hanifa <salma.hanifa04@gmail.com>
2025-05-26 14:13:12 +0700

    Update: Channge uId

--- frontend/src/auth/AuthProvider.jsx
+++ frontend/src/auth/AuthProvider.jsx
@@ -23,13 +23,7 @@ export const AuthProvider = ({ children }) => {
     try {
       const res = await axios.post(`${API_URL}/login`, { email, password });
       const token = res.data.accessToken;
-      const uid = res.data.uId;
       setAccessToken(token);
-
-      Cookies.set("uId", uid, {
-        secure: false,
-        sameSite: "None",
-      });
 
       Cookies.set("refreshToken", res.data.refreshToken, {
         secure: false,
